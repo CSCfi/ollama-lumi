a7c187c6e4d2d8ec9203285cc506aa37e7519e03
Lukas Prediger <lukas.prediger@csc.fi>
2024-08-07 12:28:33 +0000

    Patching base image to rockylinux:9.5 instead of centos7.
    CentOS 7 is end-of-life and is therefore very hard to install extra packages on.
    The included Python version is also quite old.

--- build.sh
+++ build.sh
@@ -8,9 +8,12 @@ set -xe
 
 OLLAMA_VERSION=v0.3.2
 LUMI_ROCM_VERSION=5.6.1  # this is the last ROCM version compatible with AMD drivers on LUMI
-DOCKER_IMAGE_NAME=csc/ollama-lumi
+DOCKER_BASE_IMAGE_NAME=csc/lumi/rocm
+DOCKER_IMAGE_NAME=csc/lumi/ollama
 SINGULARITY_IMAGE_NAME=ollama-lumi-${OLLAMA_VERSION}.sif
 
+# Build LUMI ROCM base image
+docker build -t $DOCKER_BASE_IMAGE_NAME:$LUMI_ROCM_VERSION --build-arg ROCM_VERSION=$LUMI_ROCM_VERSION -f LUMI-ROCM-Dockerfile .
 
 # Clone ollama codebase
 rm -rf buildtmp
@@ -25,10 +28,10 @@ trap 'popd; rm -rf buildtmp' EXIT
 git checkout $OLLAMA_VERSION
 
 # Apply code patches to Ollama that are required to run on LUMI
-git apply ../../lumi-patches/*
+git apply ../../lumi-patches/*.patch
 
 # Build Ollama ROCM docker image
-docker build -t $DOCKER_IMAGE_NAME:$LUMI_ROCM_VERSION --target runtime-rocm --build-arg AMDGPU_TARGETS="gfx90a:sramecc+:xnack-" --build-arg ROCM_VERSION=$LUMI_ROCM_VERSION .
+docker build -t $DOCKER_IMAGE_NAME:$LUMI_ROCM_VERSION --target runtime-rocm --build-arg AMDGPU_TARGETS="gfx90a:sramecc+:xnack-" --build-arg ROCM_VERSION=$LUMI_ROCM_VERSION --build-arg ROCM_BASE_IMAGE=$DOCKER_BASE_IMAGE_NAME:$LUMI_ROCM_VERSION .
 
 # Convert docker to singularity/apptainer image for LUMI
 singularity build ../../$SINGULARITY_IMAGE_NAME docker-daemon://$DOCKER_IMAGE_NAME:$LUMI_ROCM_VERSION
